0863ff52a548ae8699bf05a7b27badf505f25b18
Jyrone Parker <jyrone.parker@gmail.com>
2019-03-24 18:42:46 -0400

    orders

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,9 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          parent::toArray($request),
+          'orders' => []
+        ];
     }
 }
