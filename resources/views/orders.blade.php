b272619a4ae144dc889c79033b0e96461426b71e
Jyrone Parker <jyrone.parker@gmail.com>
2019-07-01 17:38:13 -0400

    add queued orders

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <orders></orders>
+            <queued-orders></queued-orders>
         </div>
     </div>
 </div>
