7e11df6aa847c94d057fb2d7664055bcf1a0a422
Jyrone Parker <jyrone.parker@gmail.com>
2019-09-30 19:55:33 -0400

    locations

---
+++ resources/views/locations.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <locations></locations>
+        </div>
+    </div>
+</div>
+
+@endsection
