9c0eceed9e78259b4a75b967c301eb310617675d
Jyrone Parker <jyrone.parker@gmail.com>
2020-01-29 20:13:36 -0500

    Change headline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
 					<header id="header" class="alt">
 						<span class="logo"><img src="{{url('/logo.svg')}}" alt="WeGotIt Logo" width="500px" height="500px" /></span>
 						<h1>{{env('APP_NAME')}}</h1>
-						<p>Get your food at your seat.</p>
+						<p>Get your food and merchandise while you enjoy the venue, entertainment and friends you came for.</p>
 					</header>
 
 				<!-- Nav -->
